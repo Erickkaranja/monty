ac7c1e199cd3970bc52cc3b9d7f6e60c1d8dc772
kema-ray <rachelkemuma99@gmail.com>
2023-01-17 16:25:18 +0300

    add prototypes for the files

--- monty.h
+++ monty.h
@@ -34,4 +34,11 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+
 #endif
