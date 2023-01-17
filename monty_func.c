31ce6519b3760f91c82296576e598d8f55969267
kema-ray <rachelkemuma99@gmail.com>
2023-01-17 17:30:19 +0300

    implement push opcodes

--- monty_func.c
+++ monty_func.c
@@ -1,1 +1,20 @@
-#include "mounty.h"
+#include "monty.h"
+
+/**
+  * push - pushes an element to the stack
+  * @stack: double pointer to the first node
+  * @line_number: value of new node
+  */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *element = malloc(sizeof(stack_t));
+	char *op;
+	int i;
+
+	if (element == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+}
